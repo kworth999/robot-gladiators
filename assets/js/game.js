873f1cd15580c68ffe0ca7d3bc41abc283a452dd
kworth999 <kaylaworth999@gmail.com>
2021-08-17 13:58:50 -0600

    for loop for multiple battles

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,8 +13,9 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
+  while(enemyHealth > 0) {
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -65,11 +66,15 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+  }
 };
 
 // run fight function to start game
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
+
 
   
